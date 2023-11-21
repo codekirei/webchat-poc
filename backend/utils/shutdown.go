70b4de2c9001a2d84930965b7228450969899e23
Jacob Blakely <jacob@codekirei.com>
2023-11-20 17:07:55 -0800

    Add graceful shutdown util

---
+++ backend/utils/shutdown.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func gracefulShutdown(cancel context.CancelFunc) {
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		sig := <-interruptChan
+		// TODO: wrap this in log.METHOD
+		fmt.Printf("%s received.\n", sig.String())
+		// TODO: shut down any necessary things i.e. server
+		cancel()
+	}()
+}
